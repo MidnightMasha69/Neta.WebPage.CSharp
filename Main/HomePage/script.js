87432a2c48cc425e83a1d51d8e5a65739dcf21ad
MidnightMasha69 <israelizik@gmail.com>
2018-06-19 23:56:47 +0300

    Add files via upload

--- Main/HomePage/script.js
+++ Main/HomePage/script.js
@@ -1,3 +1,6 @@
 function Load() {
-
+    //Makes #w-text's width to #selfpic's width.
+    var selfpic = document.getElementById("selfpic");
+    var wtext = document.getElementById("w-text");
+    wtext.style.width = selfpic.clientWidth + "px";
 }
