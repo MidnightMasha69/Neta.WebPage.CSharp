f7545777180a7a0b0ef3f191c82dbc04f9feb2fd
MidnightMasha69 <israelizik@gmail.com>
2018-06-30 06:56:43 +0300

    Add files via upload

--- Main/HomePage/script.js
+++ Main/HomePage/script.js
@@ -5,6 +5,23 @@
     var wquote = document.getElementById("w-quote");
     //wtext.style.width = selfpic.clientWidth + "px";
     wquote.style.width = selfpic.clientWidth + "px";
-}*/
+}
 var cs = document.getElementById("codeskills");
-var tb = document.getElementById("table");
+var tb = document.getElementById("table");*/
+function SignUp(){
+    var fname = document.getElementById("r-fname");
+    var lname = document.getElementById("r-lname");
+    localStorage.setItem("fname", fname.value);
+    localStorage.setItem("lname", lname.value);
+}
+function SignIn(){
+    if(document.getElementById("s-fname").value == localStorage.getItem("fname")){
+        document.getElementById("sign-flname").innerHTML = "welcome " + localStorage.getItem("fname") + " !";
+    }
+    else{
+        document.getElementById("sign-flname").innerHTML = "error";
+    }
+}
+function Delete(){
+    localStorage.clear();
+}
