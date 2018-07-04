c0da4f3be5d6c6fb7f7631b3eadc94a6e3269af1
MidnightMasha69 <israelizik@gmail.com>
2018-07-04 13:50:06 +0300

    Add files via upload

---
+++ Main/C#/NahasBlyat/NahasBlyat/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NahasBlyat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Nahas1.Visibility = Visibility.Visible;
+            Nahas2.Visibility = Visibility.Hidden;
+            Nahas4.Visibility = Visibility.Hidden;
+            Nahas5.Visibility = Visibility.Hidden;
+            Nahas3.Visibility = Visibility.Hidden;
+            About.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Nahas2.Visibility = Visibility.Visible;
+            Nahas4.Visibility = Visibility.Hidden;
+            Nahas1.Visibility = Visibility.Hidden;
+            Nahas5.Visibility = Visibility.Hidden;
+            Nahas3.Visibility = Visibility.Hidden;
+            About.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Nahas4.Visibility = Visibility.Hidden;
+            Nahas2.Visibility = Visibility.Hidden;
+            Nahas1.Visibility = Visibility.Hidden;
+            Nahas5.Visibility = Visibility.Hidden;
+            Nahas3.Visibility = Visibility.Visible;
+            About.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Nahas2.Visibility = Visibility.Hidden;
+            Nahas1.Visibility = Visibility.Hidden;
+            Nahas3.Visibility = Visibility.Hidden;
+            Nahas5.Visibility = Visibility.Hidden;
+            Nahas4.Visibility = Visibility.Visible;
+            About.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Nahas2.Visibility = Visibility.Hidden;
+            Nahas1.Visibility = Visibility.Hidden;
+            Nahas3.Visibility = Visibility.Hidden;
+            Nahas4.Visibility = Visibility.Hidden;
+            Nahas5.Visibility = Visibility.Visible;
+            About.Visibility = Visibility.Hidden;
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            Nahas2.Visibility = Visibility.Hidden;
+            Nahas1.Visibility = Visibility.Hidden;
+            Nahas3.Visibility = Visibility.Hidden;
+            Nahas4.Visibility = Visibility.Hidden;
+            Nahas5.Visibility = Visibility.Hidden;
+            About.Visibility = Visibility.Visible;
+        }
+    }
+}
